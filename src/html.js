54ec54364376b4a5bc665e7a35d3966488482a6c
Soman95 <s.o.ibrahim95@hotmal.com>
2021-05-02 00:45:50 +0100

    preview image change

--- src/html.js
+++ src/html.js
@@ -19,6 +19,7 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta property="og:image" content="https://drive.google.com/file/d/1ABiwkAA6UCnL2g-83VNXSvZ3ounYp8O7/view?usp=sharing" />
         {headComponents}
       </head>
       <body {...bodyAttributes}>

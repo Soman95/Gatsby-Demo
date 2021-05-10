9bbb2d092553e0655c36ca0af4c4d3a7c994827a
Soman95 <s.o.ibrahim95@hotmal.com>
2021-05-10 04:01:20 +0100

    col

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -15,36 +15,36 @@ const Skills = () => {
                     <container fluid className="skillsSection">
 
                         <row className="skillsContainer">
-                            <div className="skillsBox">
+                            <col className="skillsBox">
                                 <div className="skillsTitle">
                                     <div className="skillsImg">
                                         <img src="https://cdn.freebiesupply.com/logos/large/2x/html-5-logo-png-transparent.png" className="skillsIcons"></img>
                                     </div>
                                     <h3>HTML 5</h3>
                                 </div>
-                            </div>
+                            </col>
                         </row>
 
                         <row className="skillsContainer">
-                            <div className="skillsBox">
+                            <col className="skillsBox">
                                 <div className="skillsTitle">
                                     <div className="skillsImg">
                                         <img src="https://upload.wikimedia.org/wikipedia/commons/3/3d/CSS.3.svg" className="skillsIcons"></img>
                                     </div>
                                     <h3>CSS 3</h3>
                                 </div>
-                            </div>
+                            </col>
                         </row>
 
                         <row className="skillsContainer">
-                            <div className="skillsBox">
+                            <col className="skillsBox">
                                 <div className="skillsTitle">
                                     <div className="skillsImg">
                                         <img src="https://icon-library.net/images/javascript-icon-png/javascript-icon-png-23.jpg" className="skillsIcons"></img>
                                     </div>
                                     <h3>JavaScript</h3>
                                 </div>
-                            </div>
+                            </col>
                         </row>
                     </container>
                 </Fade>
